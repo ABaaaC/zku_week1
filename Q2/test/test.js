73c50d8632a0ee96b899f0157d87a00f1c0f9d6c
Stanislav <rus.abaaac@gmail.com>
2022-05-10 02:38:38 +0300

    added full solution for assignment

--- Q2/test/test.js
+++ Q2/test/test.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const fs = require("fs");
-const { groth16 } = require("snarkjs");
+const { groth16, plonk } = require("snarkjs");
 
 function unstringifyBigInts(o) {
     if ((typeof(o) == "string") && (/^[0-9]+$/.test(o) ))  {
@@ -35,22 +35,22 @@ describe("HelloWorld", function () {
 
     it("Should return true for correct proof", async function () {
         //[assignment] Add comments to explain what each line is doing
-        const { proof, publicSignals } = await groth16.fullProve({"a":"1","b":"2"}, "contracts/circuits/HelloWorld/HelloWorld_js/HelloWorld.wasm","contracts/circuits/HelloWorld/circuit_final.zkey");
+        const { proof, publicSignals } = await groth16.fullProve({"a":"1","b":"2"}, "contracts/circuits/HelloWorld/HelloWorld_js/HelloWorld.wasm","contracts/circuits/HelloWorld/circuit_final.zkey"); // generates a zk Proof using input parameters. 
+        
+        console.log('1x2 =',publicSignals[0]); // print string and output of the circuit
 
-        console.log('1x2 =',publicSignals[0]);
+        const editedPublicSignals = unstringifyBigInts(publicSignals); // get the BigInt values from the circuit output
+        const editedProof = unstringifyBigInts(proof); // get the BigInt values from the circuit witness 
+        const calldata = await groth16.exportSolidityCallData(editedProof, editedPublicSignals); // convert witness and output signals into obfuscated string, which then will be used as parameter of the verifyProof for verifier contract
 
-        const editedPublicSignals = unstringifyBigInts(publicSignals);
-        const editedProof = unstringifyBigInts(proof);
-        const calldata = await groth16.exportSolidityCallData(editedProof, editedPublicSignals);
+        const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString()); // parse the string into number arguments
     
-        const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
-    
-        const a = [argv[0], argv[1]];
-        const b = [[argv[2], argv[3]], [argv[4], argv[5]]];
-        const c = [argv[6], argv[7]];
-        const Input = argv.slice(8);
+        const a = [argv[0], argv[1]]; // assign the first pair of arguments as the first term
+        const b = [[argv[2], argv[3]], [argv[4], argv[5]]]; // assign next two pair of arguments as the second term
+        const c = [argv[6], argv[7]]; // assign fithe forth pair of arguments as the result of summation
+        const Input = argv.slice(8); // assign rest parameters (which are the encrypted circuit output) as Input 
 
-        expect(await verifier.verifyProof(a, b, c, Input)).to.be.true;
+        expect(await verifier.verifyProof(a, b, c, Input)).to.be.true; // proof verification (on the verifier side)
     });
     it("Should return false for invalid proof", async function () {
         let a = [0, 0];
@@ -63,30 +63,80 @@ describe("HelloWorld", function () {
 
 
 describe("Multiplier3 with Groth16", function () {
-
+    let Verifier;
+    let verifier;
     beforeEach(async function () {
         //[assignment] insert your script here
+        Verifier = await ethers.getContractFactory("Multiplier3Verifier");
+        verifier = await Verifier.deploy(); 
+        await verifier.deployed();
     });
 
     it("Should return true for correct proof", async function () {
         //[assignment] insert your script here
+        const { proof, publicSignals } = await groth16.fullProve({"a":"3","b":"2","c":"6"}, "contracts/circuits/Multiplier3/Multiplier3_js/Multiplier3.wasm","contracts/circuits/Multiplier3/circuit_final.zkey"); 
+        
+        console.log('3x2x6 =',publicSignals[0]);
+
+        const editedPublicSignals = unstringifyBigInts(publicSignals); // get the BigInt values from the circuit output
+        const editedProof = unstringifyBigInts(proof); // get the BigInt values from the circuit witness 
+        const calldata = await groth16.exportSolidityCallData(editedProof, editedPublicSignals); // convert witness and output signals into obfuscated string, which then will be used as parameter of the verifyProof for verifier contract
+
+        const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString()); // parse the string into number arguments
+
+        const a = [argv[0], argv[1]]; // assign the first pair of arguments as the first term
+        const b = [[argv[2], argv[3]], [argv[4], argv[5]]]; // assign next two pair of arguments as the second term
+        const c = [argv[6], argv[7]]; // assign fithe forth pair of arguments as the result of summation
+        const Input = argv.slice(8); // assign rest parameters (which are the encrypted circuit output) as Input 
+        // expect(true).to.be.true;
+
+        expect(await verifier.verifyProof(a, b, c, Input)).to.be.true; // proof verification (on the verifier side)
     });
     it("Should return false for invalid proof", async function () {
         //[assignment] insert your script here
+        let a = [0, 0];
+        let b = [[0, 0], [0, 0]];
+        let c = [0, 0];
+        let d = [0]
+        expect(await verifier.verifyProof(a, b, c, d)).to.be.false;
     });
 });
 
 
 describe("Multiplier3 with PLONK", function () {
-
+    let Verifier;
+    let verifier;
     beforeEach(async function () {
         //[assignment] insert your script here
+        Verifier = await ethers.getContractFactory("PlonkVerifier");
+        verifier = await Verifier.deploy(); 
+        await verifier.deployed();
     });
 
     it("Should return true for correct proof", async function () {
         //[assignment] insert your script here
+        const { proof, publicSignals } = await plonk.fullProve({"a":"3","b":"2","c":"6"}, "contracts/circuits/Multiplier3_plonk/Multiplier3_js/Multiplier3.wasm","contracts/circuits/Multiplier3_plonk/circuit_final.zkey"); 
+        
+        console.log('3x2x6 =',publicSignals[0]);
+
+        const editedPublicSignals = unstringifyBigInts(publicSignals); // get the BigInt values from the circuit output
+        const editedProof = unstringifyBigInts(proof); // get the BigInt values from the circuit witness 
+        const calldata = await plonk.exportSolidityCallData(editedProof, editedPublicSignals); // convert witness and output signals into obfuscated string, which then will be used as parameter of the verifyProof for verifier contract
+
+        // console.log(editedPublicSignals);
+        // console.log(editedProof);
+
+
+        // const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString()); // parse the string into number arguments
+        const a = calldata.split(',')[0];
+        const b = JSON.parse(calldata.split(',')[1]);
+
+        expect(await verifier.verifyProof(a, b)).to.be.true; // proof verification (on the verifier side)
     });
     it("Should return false for invalid proof", async function () {
         //[assignment] insert your script here
+        let a = '0x00';
+        let b = ['0'];
+        expect(await verifier.verifyProof(a, b)).to.be.false;
     });
 });
--- Q3/contracts/bonus/SystemOfEquations.circom
+++ Q3/contracts/bonus/SystemOfEquations.circom
@@ -1,7 +1,9 @@
 pragma circom 2.0.0;
 
 include "../../node_modules/circomlib/circuits/comparators.circom";
-include ""; // hint: you can use more than one templates in circomlib-matrix to help you
+// include ""; // hint: you can use more than one templates in circomlib-matrix to help you
+include "../../node_modules/circomlib-matrix/circuits/matMul.circom";
+include "../../node_modules/circomlib/circuits/comparators.circom";
 
 template SystemOfEquations(n) { // n is the number of variables in the system of equations
     signal input x[n]; // this is the solution to the system of equations
@@ -10,6 +12,32 @@ template SystemOfEquations(n) { // n is the number of variables in the system of
     signal output out; // 1 for correct solution, 0 for incorrect solution
 
     // [bonus] insert your code here
+
+    component multiplication = matMul(n,n,1);
+    // Initialization of A and x:
+    for (var i = 0; i < n; i++) {
+        for (var j = 0; j < n; j++) {
+            multiplication.a[i][j] <== A[i][j];
+        }
+        multiplication.b[i][0] <== x[i];
+    }
+
+    // Compare A[i][:]*x and b[i]
+    component equals_axb[n]; 
+    for (var i = 0; i < n; i++) {
+        equals_axb[i] = IsEqual();
+        equals_axb[i].in[0] <== multiplication.out[i][0];
+        equals_axb[i].in[1] <== b[i];
+    }
+
+    // Check all comarisons
+    signal all_equals[n];
+    all_equals[0] <== equals_axb[0].out;
+    for (var i = 1; i < n; i++) {
+        all_equals[i] <== all_equals[i-1] * equals_axb[i].out;
+    }
+    out <== all_equals[n-1];
+
 }
 
 component main {public [A, b]} = SystemOfEquations(3);
